28010b55470f505abe3c94e9e4a8f428b5912914
Levi Robertson <robertson.l.levi@gmail.com>
2018-10-09 18:45:13 -0700

    styled drip form

--- src/ui/hero.js
+++ src/ui/hero.js
@@ -6,6 +6,35 @@ import road from "../images/road.jpg";
 const Hero = styled.div``;
 const Centered = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: center;
+  h3 {
+    margin: 5px;
+  }
+`;
+
+const Button = styled.button`
+  outline: none;
+  margin: 10px auto;
+  width: 200px;
+  height: 40px;
+  text-align: center;
+  width: 130px;
+  border-radius: 40px;
+  background: #fff;
+  border: 2px solid #1ca086;
+  color: #1ca086;
+  transition: all 0.25s ease;
+  &:hover {
+    background: #1ca086;
+    color: white;
+  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
 `;
 
 export default () => (
@@ -19,16 +48,15 @@ export default () => (
       JavaScript that you can sign up for below.
     </p>
     <Centered>
-      <form
+      <Form
         action="https://www.getdrip.com/forms/428751402/submissions"
         method="post"
         data-drip-embedded-form="428751402"
       >
         <h3 data-drip-attribute="headline">Learn Functional Programming!</h3>
         <div data-drip-attribute="description" />
-        <input name="fields[email]" placeholder="Email" />
-        <button type="submit">Sign Up!</button>
-      </form>
+        <Button type="submit">Sign Up!</Button>
+      </Form>
     </Centered>
   </Hero>
 );
